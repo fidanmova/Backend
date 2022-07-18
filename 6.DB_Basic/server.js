199078480f47fe19646d9b0a0841552ab5522836
ariffira <mac.fira@gmail.com>
2022-07-18 14:34:50 +0200

    dotenv added

--- 6.DB_Basic/server.js
+++ 6.DB_Basic/server.js
@@ -3,24 +3,24 @@ const app = express();
 const PORT = 5000;
 const mongoose = require('mongoose')
 const User = require('./model/userModel')
+require('dotenv').config();
 
 // settings
+app.use(express.json())
 /**
  * Connect Database with MongoDB cloud account
  */
-mongoose.connect('mongodb+srv://testUser:test1234@arifdci.fvkse.mongodb.net/test')
+mongoose.connect(`mongodb+srv://${process.env.DB_User}:${process.env.DB_password}@arifdci.fvkse.mongodb.net/test`)
 .then(()=>{
     console.log('MongoDB is connected...')
 })
 
 // Routes
 // Create a user from User model and save it
-app.get('/user/create', (req, res)=>{
+app.post('/user/create', (req, res)=>{
     // how to create data based on model?
-    const newUser = new User({
-        first_name: 'Vassilis',
-        last_name: 'Khan'
-    })
+    console.log(req.body)
+    const newUser = new User(req.body)
     // save the data into DB using save() method
     newUser.save(()=>{
         res.json('1 User has been created')
